653d39a73e11ab7f4685f6a3586443cb31bef88a
Eric Lathrop <eric@ericlathrop.com>
2015-10-13 17:16:17 -0400

    Convert main script to new unfold

--- bin/unfold.js
+++ bin/unfold.js
@@ -1,54 +1,31 @@
 #!/usr/bin/env node
 "use strict";
 
-var path = require("path");
-var q = require("q");
-var config = require("../lib/config");
-var fsExtra = require("../lib/fs_extra");
-var loadPartials = require("../lib/load_partials");
-var loadHelpers = require("../lib/load_helpers");
-var transformHandlebars = require("../lib/transform_handlebars");
-var transformSass = require("../lib/transform_sass");
+var async = require("async");
+var fs = require("fs");
+var unfold = require("../lib/unfold");
 
-function endsWith(str, suffix) {
-	return str.indexOf(suffix, str.length - suffix.length) !== -1;
-}
-
-function processFile(cfg, src, dest) {
-	cfg = config.forSourceFile(src, cfg);
-	if (endsWith(src, ".hbs")) {
-		return transformHandlebars(src, dest, cfg);
+function asyncJsonParse(json, callback) {
+	try {
+		callback(undefined, JSON.parse(json));
+	} catch (e) {
+		callback(e);
 	}
-	if (endsWith(src, ".scss") || endsWith(src, ".sass")) {
-		if (path.basename(src)[0] === "_") {
-			return q();
-		}
-		return transformSass(src, dest);
-	}
-	return fsExtra.copy(src, dest);
-}
-
-function processSite(file) {
-	config.load(file).then(function(cfg) {
-		// FIXME: this should be taken care of inside the config module, and tested
-		if (cfg.partialsDirectory) {
-			loadPartials(cfg.partialsDirectory);
-		}
-		// FIXME: this should be taken care of inside the config module, and tested
-		if (cfg.helpersDirectory) {
-			loadHelpers(cfg.helpersDirectory);
-		}
-
-		return fsExtra.clone(cfg.sourceDirectory, cfg.destinationDirectory, processFile.bind(undefined, cfg));
-	}).done();
 }
 
 function main(argv) {
 	var args = argv.slice(2);
-
-	for (var i = 0; i < args.length; i++) {
-		processSite(args[i]);
+	if (args.length === 0) {
+		console.error("Usage: unfold config [...]");
+		return;
 	}
+	var run = async.seq(fs.readFile, asyncJsonParse, unfold);
+	async.each(args, run, function(err) {
+		if (err) {
+			console.error(err);
+			return;
+		}
+	});
 }
 
 main(process.argv);
